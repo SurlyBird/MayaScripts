f3075ab683558b01b09725c90a26fed5530d8f52
ronnie <ronnie@fedora.clementine>
2015-09-06 14:58:48 -0700

    Updating with wireframe-on-selected controls.

--- raSuperCombiner.py
+++ raSuperCombiner.py
@@ -5,6 +5,8 @@ __author__ = 'ronnie ashlock'
 import pymel.core as pmc
 import maya.mel as mel
 
+import pymel.core as pmc
+
 class SuperCombiner():
 
     def combine_all(self):
@@ -22,22 +24,42 @@ class SuperCombiner():
         pmc.polySeparate()
 
     def nuke_history(self):
-        pmc.general.delete(constructionHistory=True)
+        pmc.general.delete(constructionHistory = True)
 
     def reset_transform(self):
         pmc.makeIdentity(t = True, a = False)
 
     def freeze_transform(self):
         pmc.makeIdentity(t = True, a = True)
-        
-    def fix_pivot(self):
-        pmc.xform(centerPivots = True)
-        
-    def set_to_origin(self):
-        mel.eval('numericalInputChangeCommand abs')
+
+    def center_pivot(self):
+        pmc.xform(centerPivots=True)
+
+    def move_to_origin(self):
+        pmc.mel.eval('numericalInputChangeCommand abs')
         pmc.makeIdentity(t = True, a = True)
 
-    pmc.window(width = 200, title ='Super Combiner')
+    def wfos_on(*args):
+
+        x = pmc.mel.eval('findNewCurrentModelView')
+        active_panel = 'modelPanel%i' % (4)
+        shade_state = 'true '
+        option = 'setWireframeOnShadedOption %s %s' % (shade_state, active_panel)
+        z = 'setWireframeOnShadedOption false modelPanel4'
+
+        pmc.mel.eval(option)
+
+    def wfos_off(*args):
+
+        x = pmc.mel.eval('findNewCurrentModelView')
+        active_panel = 'modelPanel%i' % (4)
+        shade_state = 'false '
+        option = 'setWireframeOnShadedOption %s %s' % (shade_state, active_panel)
+        z = 'setWireframeOnShadedOption false modelPanel4'
+
+        pmc.mel.eval(option)
+
+    pmc.window(width=200, title='Super Combiner')
     pmc.columnLayout(adjustableColumn = True)
     pmc.button(label = "Combine Selected", command = combine_selected)
     pmc.button(label = "Separate Selected", command = separate_selected)
@@ -46,9 +68,10 @@ class SuperCombiner():
     pmc.button(label = "Reset Transforms", command = reset_transform)
     pmc.button(label = "Freeze Transforms", command = freeze_transform)
     pmc.button(label = "Delete History", command = nuke_history)
-    pmc.button(label = "Center Pivot", command = fix_pivot)
-    pmc.button(label = "Move to Origin", command=set_to_origin)
+    pmc.button(label = "Center Pivot", command = center_pivot)
+    pmc.button(label = "Move to Origin", command = move_to_origin)
+    pmc.button(label = "WFoS ON", command = wfos_on)
+    pmc.button(label = "WFoS OFF", command = wfos_off)
+
 
     pmc.showWindow()
-
-SuperCombiner()
