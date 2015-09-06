42c55b09af8b653d3f1be5b6420ee775fe161a86
Ronnie Ashlock <ronnieashlock@gmail.com>
2015-09-06 13:47:48 -0700

    Add newly renamed files.

---
+++ raSphere_Maker_Random.py
@@ -0,0 +1,19 @@
+from pymel.core import *
+import maya.cmds as mc
+import maya.mel as mel
+import pymel.core as pm
+import random
+import math
+
+
+
+win = window(title="Locator Maker")
+
+frameLayout('User_Feedback',label='Input',width=220,cll=True)
+mc.columnLayout()
+
+mc.rowLayout(numberOfColumns=1,cw1=(80))
+mc.button(label='Make Sphere',w=80, c=lambda *args:polySphere(r = random.uniform(0,12)))
+mc.setParent('..')
+
+showWindow()
---
+++ raSuperCombiner.py
@@ -0,0 +1,52 @@
+__author__ = 'ronnie ashlock'
+
+import pymel.core as pmc
+import maya.mel as mel
+
+class SuperCombiner():
+
+    def combine_all(self):
+        pmc.select(all = True)
+        pmc.polyUnite()
+
+    def combine_selected(self):
+        pmc.polyUnite()
+
+    def separate_all(self):
+        pmc.select(all = True)
+        pmc.polySeparate()
+
+    def separate_selected(self):
+        pmc.polySeparate()
+
+    def nuke_history(self):
+        pmc.general.delete(constructionHistory=True)
+
+    def reset_transform(self):
+        pmc.makeIdentity(t = True, a = False)
+
+    def freeze_transform(self):
+        pmc.makeIdentity(t = True, a = True)
+        
+    def fix_pivot(self):
+        pmc.xform(centerPivots = True)
+        
+    def set_to_origin(self):
+        mel.eval('numericalInputChangeCommand abs')
+        pmc.makeIdentity(t = True, a = True)
+
+    pmc.window(width = 200, title ='Super Combiner')
+    pmc.columnLayout(adjustableColumn = True)
+    pmc.button(label = "Combine Selected", command = combine_selected)
+    pmc.button(label = "Separate Selected", command = separate_selected)
+    pmc.button(label = "Combine All", command = combine_all)
+    pmc.button(label = "Separate All", command = separate_all)
+    pmc.button(label = "Reset Transforms", command = reset_transform)
+    pmc.button(label = "Freeze Transforms", command = freeze_transform)
+    pmc.button(label = "Delete History", command = nuke_history)
+    pmc.button(label = "Center Pivot", command = fix_pivot)
+    pmc.button(label = "Move to Origin", command=set_to_origin)
+
+    pmc.showWindow()
+
+SuperCombiner()
