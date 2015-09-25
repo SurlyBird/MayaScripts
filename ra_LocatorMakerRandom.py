784856b09623d5c385613744a412f8c2f998d16f
ronnie <ronnie@fedora.clementine>
2015-09-25 13:14:06 -0700

    Updating script and beautifying code.

--- ra_LocatorMakerRandom.py
+++ ra_LocatorMakerRandom.py
@@ -6,14 +6,11 @@ import pymel.core as pmc
 import random
 import math
 
-win = window(title="Locator Maker")
-
-frameLayout('User_Feedback',label='Input',width=220,cll=False)
-pmc.columnLayout()
-
-pmc.rowLayout(numberOfColumns=1,cw1=(80))
-pmc.button(label='Make Locator',w=80, c=lambda *args:(spaceLocator(p = [random.uniform(0,12), random.uniform(0,12), random.uniform(0,12)]), CenterPivot()))
+pmc.window(title="Locator Maker")
+pmc.frameLayout('User_Feedback',label='Input',width=200,cll=False)
+pmc.columnLayout(bgc=(0.5, 0.0, 0.0))
+pmc.rowLayout(numberOfColumns=1, bgc=(0.1, 0.5, 0.25), cal=(1, 'center'))
+pmc.button(label='Make a Randomly-Placed Locator',w=200, c=lambda *args:(pmc.spaceLocator(p = [random.uniform(0,12), random.uniform(0,12), random.uniform(0,12)]), pmc.xform(centerPivots=True)))
 pmc.setParent('..')
 
-showWindow()
-
+pmc.showWindow()
