284255a9b987cb8a1702440cb7512b7b75e8cd9d
ronnie <ronnie@fedora.clementine>
2015-09-25 12:32:46 -0700

    Updating script with class structure and methods.

--- ra_SphereMakerMoverRandom.py
+++ ra_SphereMakerMoverRandom.py
@@ -1,19 +1,25 @@
 __author__ = 'ronnie ashlock'
 
-# A learning script that makes a sphere and moves it to a random position. Whee!
+# A learning script that makes a sphere at random sizes. Whee!
 
 import pymel.core as pmc
 import random
 import math
 
-win = window(title="Sphere Randomizer")
-
-frameLayout('User_Feedback',label='Input',width=220,cll=True)
-pmc.columnLayout()
-
-pmc.rowLayout(numberOfColumns=2,cw1=(80))
-pmc.button(label='Make Sphere',w=80, c=lambda *args:(polySphere(r = random.uniform(0,12)), move(random.uniform(-20.0,20), random.uniform(-20.0,20), random.uniform(-20.0,20)))    )
-pmc.button(label='Move Sphere',w=80, c=lambda *args:move(random.random(), random.random(), random.random()))
-pmc.setParent('..')
-
-showWindow()
+class SphereMakerMoverRandom():
+    def sphere_maker(self):
+        pmc.polySphere(r = random.uniform(0,12))
+        pmc.setParent('..')
+    def sphere_mover(self):
+        pmc.move([random.random(), random.random(), random.random()])
+        pmc.setParent('..')
+        
+     
+    pmc.window(width=200, title="Sphere Randomizer")
+    pmc.columnLayout(adjustableColumn = True, bgc=(0.5, 0.0, 0.5))
+    pmc.rowLayout(numberOfColumns=2, bgc=(0.25, 0.75, 0.1), cw2=(100,100), cal=(2,'left'))
+    pmc.button(label="Make Sphere", command=sphere_maker)
+    pmc.button(label="Move Sphere", command=sphere_mover)
+    
+    
+    pmc.showWindow()
