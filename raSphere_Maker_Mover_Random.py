d8f01cead6b4c1e20da1048fb87dd85d5d12ac7b
Ronnie Ashlock <ronnieashlock@gmail.com>
2015-09-25 11:16:13 -0700

    Update raSphere_Maker_Mover_Random.py

--- raSphere_Maker_Mover_Random.py
+++ raSphere_Maker_Mover_Random.py
@@ -2,21 +2,18 @@ __author__ = 'ronnie ashlock'
 
 # A learning script that makes a sphere and moves it to a random position. Whee!
 
-from pymel.core import *
-import maya.cmds as mc
-import maya.mel as mel
-import pymel.core as pm
+import pymel.core as pmc
 import random
 import math
 
 win = window(title="Sphere Randomizer")
 
 frameLayout('User_Feedback',label='Input',width=220,cll=True)
-mc.columnLayout()
+pmc.columnLayout()
 
-mc.rowLayout(numberOfColumns=2,cw1=(80))
-mc.button(label='Make Sphere',w=80, c=lambda *args:(polySphere(r = random.uniform(0,12)), move(random.uniform(-20.0,20), random.uniform(-20.0,20), random.uniform(-20.0,20)))    )
-mc.button(label='Move Sphere',w=80, c=lambda *args:move(random.random(), random.random(), random.random()))
-mc.setParent('..')
+pmc.rowLayout(numberOfColumns=2,cw1=(80))
+pmc.button(label='Make Sphere',w=80, c=lambda *args:(polySphere(r = random.uniform(0,12)), move(random.uniform(-20.0,20), random.uniform(-20.0,20), random.uniform(-20.0,20)))    )
+pmc.button(label='Move Sphere',w=80, c=lambda *args:move(random.random(), random.random(), random.random()))
+pmc.setParent('..')
 
 showWindow()
