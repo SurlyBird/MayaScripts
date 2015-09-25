e575b8cc88ef029b0f2aadbbcd9daf61ed89fb42
Ronnie Ashlock <ronnieashlock@gmail.com>
2015-09-25 11:14:30 -0700

    Update raSphere_Maker_Random.py

--- raSphere_Maker_Random.py
+++ raSphere_Maker_Random.py
@@ -2,20 +2,17 @@ __author__ = 'ronnie ashlock'
 
 # A learning script that makes a sphere at random sizes. Whee!
 
-from pymel.core import *
-import maya.cmds as mc
-import maya.mel as mel
-import pymel.core as pm
+import pymel.core as pmc
 import random
 import math
 
 win = window(title="Locator Maker")
 
 frameLayout('User_Feedback',label='Input',width=220,cll=True)
-mc.columnLayout()
+pmc.columnLayout()
 
-mc.rowLayout(numberOfColumns=1,cw1=(80))
-mc.button(label='Make Sphere',w=80, c=lambda *args:polySphere(r = random.uniform(0,12)))
-mc.setParent('..')
+pmc.rowLayout(numberOfColumns=1,cw1=(80))
+pmc.button(label='Make Sphere',w=80, c=lambda *args:polySphere(r = random.uniform(0,12)))
+pmc.setParent('..')
 
 showWindow()
