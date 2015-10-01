1c2eccbd67b83a9b8c09e5cc7699289ac0763065
Ronnie Ashlock <ronnieashlock@gmail.com>
2015-10-01 15:55:10 -0700

    Update ra_LocatorMakerRandom.py
    Added a better title.

--- ra_LocatorMakerRandom.py
+++ ra_LocatorMakerRandom.py
@@ -6,7 +6,7 @@ import pymel.core as pmc
 import random
 import math
 
-pmc.window(title="Locator Maker")
+pmc.window(title="Random Locator Maker")
 pmc.frameLayout('User_Feedback',label='Input',width=200,cll=False)
 pmc.columnLayout(bgc=(0.5, 0.0, 0.0))
 pmc.rowLayout(numberOfColumns=1, bgc=(0.1, 0.5, 0.25), cal=(1, 'center'))
